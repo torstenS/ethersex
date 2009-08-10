16d87e2e077575f67eac24c2af4045f78479ef21
Stefan Siegl <stesie@brokenpipe.de>
2009-08-10 10:30:29 +0200

    config.h: set EEPROM_SUPPORT if KTY_SUPPORT is set.

--- config.h
+++ config.h
@@ -134,7 +134,7 @@
    - STELLA with eeprom load/write support */
 #if (defined(ECMD_PARSER_SUPPORT) && (!defined(TEENSY_SUPPORT)))  \
   || (defined(BOOTP_SUPPORT) && defined(BOOTP_TO_EEPROM_SUPPORT)) \
-  || defined(STELLA_EEPROM)
+  || defined(STELLA_EEPROM) || defined(KTY_SUPPORT)
 #  define EEPROM_SUPPORT 1
 #endif
 
